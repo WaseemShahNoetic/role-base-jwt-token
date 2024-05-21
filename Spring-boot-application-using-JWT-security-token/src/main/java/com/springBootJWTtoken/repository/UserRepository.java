96a97d61dce069f22912ff7e28a808a66fed0405
WaseemShahNoetic <waseem@noeticworld.com>
2024-05-21 02:39:41 -0700

    Add files via upload

---
+++ Spring-boot-application-using-JWT-security-token/src/main/java/com/springBootJWTtoken/repository/UserRepository.java
@@ -0,0 +1,11 @@
+//package com.springBootJWTtoken.repository;
+//
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Repository;
+//
+//@Repository
+//public interface UserRepository extends CrudRepository<UserEntity, Long> {
+//
+//    UserDTO findByUsername(String username);
+//}
